b3e4c43ce7cc303d80ed1be26452c08682723e60
tiagobalm <tiagoalmeida.95@hotmail.com>
2016-10-31 10:18:59 +0000

    Project organized

---
+++ Nodes/src/nodes.pl
@@ -0,0 +1,61 @@
+:-use_module(library(lists)).
+
+board([
+        [null, null, roofL, empty, empty, empty, empty, empty, roofR, null, null],
+        [null, roofL, empty, empty, empty, empty, empty, empty, empty, roofR, null],
+        [roofLM, empty, empty, empty, empty, empty, empty, empty, empty, empty, null],
+        [roofLM, empty, empty, empty, empty, empty, empty, empty, empty, empty, null],
+        [roofLM, empty, empty, empty, empty, empty, empty, empty, empty, empty, null],
+        [roofLM, empty, empty, empty, empty, empty, empty, empty, empty, empty, null],
+        [roofLM, empty, empty, empty, empty, empty, empty, empty, empty, empty, null],
+        [null, roofLM, empty, empty, empty, empty, empty, empty, empty, null, null],
+        [null, null, roofLM, empty, empty, empty, empty, empty, null, null, null]
+       ]).
+
+translateContent(null, '    ').
+translateContent(empty, '   |').
+translateContent(unit1, ' x |').
+translateContent(unit2, ' o |').
+translateContent(node1, ' 1 |').
+translateContent(node2, ' 2 |').
+translateContent(roofL, '    |').
+translateContent(roofLM, '    |').
+translateContent(roofR, '').
+
+translateBottom(null, '    ').
+translateBottom(X, '___|') :-
+        member(X, [empty, unit1, unit2, node1, node2]).
+translateBottom(roofL, ' ___|').
+translateBottom(roofLM, '    |').
+translateBottom(roofR, '___').
+
+displayLineBottom([]) :-
+        write('').
+
+displayLineBottom([E1|Es]) :-
+        translateBottom(E1, V),
+        write(V),
+        displayLineBottom(Es).
+
+displayLine([]) :-
+        write('').
+
+displayLine([E1|Es]) :-
+        translateContent(E1, V),
+        write(V),
+        displayLine(Es).    
+
+displayBoardAux([]) :-
+        displayLine([]).
+
+displayBoardAux([L1|Ls]):-
+        displayLine(L1),
+        nl,
+        displayLineBottom(L1),
+        nl,
+        displayBoardAux(Ls).
+
+displayBoard(X) :-
+        write('             ___ ___ ___ ___ ___'),
+        nl,
+        displayBoardAux(X).
