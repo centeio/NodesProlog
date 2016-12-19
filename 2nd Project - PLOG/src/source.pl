15ad5ba22d83ec139ce99a4daef553fa53ef073b
tiagobalm <tiagoalmeida.95@hotmail.com>
2016-12-19 12:44:52 +0000

    Second Project

---
+++ 2nd Project - PLOG/src/source.pl
@@ -0,0 +1,5 @@
+:-include('Database.pl').
+:-use_module(library(clpfd)).
+:-use_module(library(lists)).
+:-set_prolog_flag(toplevel_print_options,
+                  [quoted(true), portrayed(true), max_depth(0)]).
