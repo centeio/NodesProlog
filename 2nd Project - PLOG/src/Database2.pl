a2aab92861bc8cd9e92708ea0e565db5d2886a37
centeio <c.centeiojorge@outlook.pt>
2016-12-21 17:27:12 +0000

    Fim

--- 2nd Project - PLOG/src/Database2.pl
+++ 2nd Project - PLOG/src/Database2.pl
@@ -1,38 +1,35 @@
-distanceTab([[1,10],
-          [0,10],
-          [0,0]]).
+distanceTab([[1,10,10,10,10],
+             [10,1,20,10],
+                [1,10,10],
+                [10,1],
+                [10]]).
            
-distance(1, 2, 0).
-%distance(1, 3, 10).
-distance(1, 6, 1).
-%distance(1, industry40, 1).
-distance(1, 5, 1).
-distance(1, 4, 1).
+distance(1, 2, 1).
+distance(1, 3, 10).
+distance(1, 6, 10).
+distance(1, 5, 10).
+distance(1, 4, 10).
 
-%distance(2, 3, 10).
-distance(2, 6, 1).
-%distance(2, industry40, 1).
-distance(2, 5, 1).
+distance(2, 3, 10).
+distance(2, 6, 10).
+distance(2, 5, 20).
 distance(2, 4, 1).
 
-%distance(3, cloudhosting, 10).
-%distance(3, industry40, 10).
-%distance(3, 5, 10).
-%distance(3, 4, 10).
+distance(3, 6, 10).
+distance(3, 5, 10).
+distance(3, 4, 1).
 
-%distance(cloudhosting, industry40, 10).
-distance(6, 5, 1).
-distance(6, 4, 1).
+distance(4, 5, 10).
+distance(4, 6, 1).
 
-%distance(industry40, 5, 10).
-%distance(industry40, 4, 10).
-
-distance(5, 4, 1).
+distance(5, 6, 10).
 
 night(100).
 
-budget(1000).
+budget(3000).
 
+genero(-1, 'Female').
+genero(1, 'Male').
 
 %speaker(nome, genero, topico, pais, precoviagem, preco noite/pack)
 %speaker(rui, 1, ai, portugal, 200, 200). %400
@@ -42,11 +39,29 @@ budget(1000).
 %speaker(pierre, 1, remoteaccess, france, 100, 2). %200
 %speaker(li, 0, cloushosting, china, 800, 1). %1000
 
-speaker(1, 1, 1, 1, 200, 200). %400
-speaker(2, 0, 2, 6, 200, 100). %300
-speaker(3, 1, 3, 2, 700, 100). %800
-speaker(4, 0, 4, 3, 500, 100). %600
-speaker(5, 1, 5, 4, 100, 200). %300
-speaker(6, 0, 6, 5, 800, 100). %900
+speaker(1, 1, 1, 1, 200, 1). %400
+speaker(2, -1, 2, 1, 200, 2). %300
+speaker(3, 1, 3, 2, 700, 1). %800
+speaker(4, -1, 4, 3, 500, 1). %600
+speaker(5, 1, 5, 4, 100, 1). %300
+speaker(6, -1, 6, 5, 800, 2). %900
 
+nome(1, 'Rui').
+nome(2, 'Sofia').
+nome(3, 'Will').
+nome(4, 'Sarra').
+nome(5, 'Pierre').
+nome(6, 'Li').
 
+pais(1, 'Portugal').
+pais(2, 'USA').
+pais(3, 'Tunisia').
+pais(4, 'France').
+pais(5, 'China').
+
+topic(1, 'Artificial Intelligence').
+topic(2, 'Machine Learning').
+topic(3, 'Web Security').
+topic(4, 'Virtual Reality').
+topic(5, 'Remote Access').
+topic(6, 'Cloud Hosting').
--- 2nd Project - PLOG/src/source.pl
+++ 2nd Project - PLOG/src/source.pl
@@ -5,28 +5,80 @@
                   [quoted(true), portrayed(true), max_depth(0)]).
 
 
-invited(Invited):-
-        Invited = [_,_,_],
+invited(Invited,NPalestras, NDias):-
+        createSpeakerArrays(Speakers, Topics, Countries, Gen, Desloc, Aloj),
+        createProximityMatrix(Topics, Dists),
+        length(Speakers, Len),
+        length(Invited, Len),
         domain(Invited,0,1),
-        Countries = [1,2,1],
-        Gen = [1,-1,1],
-%        Topics = [1,2,3],
-        distanceTab(Dists),
-        length(Invited,Len),
-        length(Sums,Len),
+        night(DPrice),
+        calcDiasAloj(Aloj, NDias, DiasAloj),
+        scalar_product(Desloc,Invited,#=,AllDesloc),
+        scalar_product(DiasAloj,Invited,#=,AllAloj),
+        budget(Budget),
+        (AllDesloc + (AllAloj * DPrice)) #=< Budget,
+        SumLen is Len-1,
+        length(Sums,SumLen),
         calcDist(Invited,Dists,Sums),
         sum(Sums,#=,Total),
         scalar_product(Gen,Invited,#=,0),
-%       sum(Invited,#=,NPalestras),
-        alldif1(Invited,Countries),        
-        labeling([maximize(Total)],Invited).
+        sum(Invited,#=,NPalestras),
+        alldif1(Invited,Countries),
+        statistics(walltime,_),    
+        labeling([], Invited),
+        statistics(walltime,[_,T]),
+        write('Time: '), write(T), nl,
+        fd_statistics,
+        write('Topics distance: '), write(Total),nl,
+        write('Despesa alojamento: '), write(AllAloj * DPrice),nl,
+        write('Despesa deslocamento: '), write(AllDesloc), nl,
+        printResult(Invited, Speakers, Topics, Countries, Gen, Desloc, DiasAloj, DPrice).
 
-calcDist([],[],[]).
+createProximityMatrix([], []).
+createProximityMatrix([T1|Ts], [D1|Ds]) :-
+        getDistLine(T1, Ts, D1),
+        createProximityMatrix(Ts,Ds).
+
+getDistLine(_, [], []).
+getDistLine(T2, [T1|Ts], [D1|Ds]) :-
+       distance(T2, T1, D1),
+       getDistLine(T2, Ts, Ds). 
+
+printResult([0|Is], [_|Ss], [_|Ts], [_|Cs], [_|Gs], [_|Ds], [_|As], DPrice) :-
+        printResult(Is, Ss, Ts, Cs, Gs, Ds, As, DPrice).
+
+printResult([1|Is], [S1|Ss], [T1|Ts], [C1|Cs], [G1|Gs], [D1|Ds], [A1|As], DPrice) :-
+        nome(S1, Nome),
+        pais(C1, Pais),
+        genero(G1, Genero),
+        topic(T1, Topic),
+        Aloj is A1 * DPrice,
+        write(Nome), write(' - '), write(Genero), write(' - '), write(Pais),
+        write(' - '), write(Topic), write(' - '), write(D1), write(' - '), 
+        write(Aloj), nl,
+        printResult(Is, Ss, Ts, Cs, Gs, Ds, As, DPrice).
+        
+createSpeakerArrays(Speakers, Topics, Countries, Gen, Desloc, Aloj) :-
+        findall(Speaker, speaker(Speaker,_,_,_,_,_), Speakers),
+        findall(Topic, speaker(_,_,Topic,_,_,_), Topics),
+        findall(Country, speaker(_,_,_,Country,_,_), Countries),
+        findall(Gender, speaker(_,Gender,_,_,_,_), Gen),
+        findall(D, speaker(_,_,_,_,D,_), Desloc),
+        findall(A, speaker(_,_,_,_,_,A), Aloj).
+
+calcDiasAloj([],_,[]).
+calcDiasAloj([A1|As], NDias, [D1|Ds]) :-
+        (A1 #= 1) #=> D1 #= NDias,
+        (A1 #= 2) #=> D1 #= 1,
+        calcDiasAloj(As, NDias, Ds).
+
+calcDist(_,[[]|_],_).
 
 calcDist([I1|Is],[D1|Ds],[S1|Ss]):-
-        (I1 #= 1) #=> scalar_product(D1,Is,#=,S1),
-        (I1 #= 0) #=> S1 #=0,
+        scalar_product(D1,Is,#=,Sum),
+        S1 #= Sum * I1,
         calcDist(Is,Ds,Ss).
+
 
 alldif1([],[]).
 alldif1([Inv|RInv],[Country|RCountries]):-
@@ -37,6 +89,3 @@ alldif1_aux(_,[],_,[]).
 alldif1_aux(Inv,[Inv2|RInv],Country,[Country2|RCountries]):-
         (Inv#=1 #/\ Inv2#=1) #=> (Country #\= Country2),
          alldif1_aux(Inv,RInv,Country,RCountries).
-
-
-               
